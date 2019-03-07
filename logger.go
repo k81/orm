3af47b5b14fac17f48b85d1cf21b1f4e7e563616
kaizhangkai <longbowk@gmail.com>
2019-03-08 00:28:35 +0800

    update

--- logger.go
+++ logger.go
@@ -2,7 +2,7 @@ package orm
 
 import "github.com/k81/log"
 
-var logger *log.Logger = log.DefaultLogger
+var logger *log.Logger = log.GetLogger()
 
 // SetLogger set the orm logger
 func SetLogger(l *log.Logger) {
